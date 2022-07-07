3956e7d40ca6c8779f27f66d859d4ae9aa0fb408
PhamXuanDong <pxd1491.forwork@gmail.com>
2022-07-07 17:29:52 +0700

    add to dev branch

--- Employee.rb
+++ Employee.rb
@@ -1,16 +1,15 @@
 class Employee
-    attr_accessor :id, :name, :pos
+  attr_accessor :id, :name, :pos
 
-    def initialize id, name, pos
-        self.id = id
-        self.name = name
-        self.pos = pos
-    end
+  def initialize(id, name, pos)
+    self.id = id
+    self.name = name
+    self.pos = pos
+  end
 
-    def to_s
-        "ID: #{id} | Name: #{name} | Position: #{pos}"
-    end
-
+  def to_s
+    "ID: #{id} | Name: #{name} | Position: #{pos}"
+  end
 end
 
 a = Employee.new 1, 'An', 'dev'
